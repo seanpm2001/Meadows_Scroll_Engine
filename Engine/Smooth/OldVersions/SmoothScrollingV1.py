e4fdb3fa0c20c16fbbd26236f5c9c2171830e32a
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-01-13 13:33:39 -0800

    SmoothScrollingV1.py
    An archive of the first version of the smooth scrolling engine script for Meadows.

---
+++ Engine/Smooth/OldVersions/SmoothScrollingV1.py
@@ -0,0 +1,22 @@
+# Start of script
+'''
+Engine for Smooth scrolling on Meadows
+'''
+# Definition section
+def smoothScrollingDef():
+  print ("Smooth scrolling is the effect of clearness when scrolling is done at a high frame rate")
+smoothScrollingMin = float(50.00) # Minimum: 50 frames per second
+smoothScrollingMax = bool("false")
+# Drivers
+""" Not yet available"""
+# Output
+print (smoothScrollingDef())
+noMore = input("This file is not meant to be opened. Press [ENTER] key to quit")
+print ("The program should now be closed. If the program is still open, close the window. If this doesn't work, end the process for this file with a process manager")
+'''
+File info
+File type: Python script file (*.py)
+Line count including blank lines and compiler line): 23
+File version: 1 (Wednesday, January 13th 2021 at 1:33 pm)
+'''
+# End of script
